a182b6f48a3595a2cc51a8bc8f253e20755012a4
ooopses <efin.u@yandex.ru>
2016-08-06 20:31:39 +0300

    Правки, удалил assets, не юзается

--- views/show_banner.php
+++ views/show_banner.php
@@ -7,7 +7,13 @@ $colors = [
 <?php for ($i = 0; $i <= $count; $i++): ?>
 
     <div style="display:block;width:100px;height:75px;border:1px solid <?= $colors[array_rand($colors)] ?>;"> 
-        <?= file_get_contents(Yii::getAlias($banner['file'])) ?>
+        <?=
+        strtr(file_get_contents(Yii::getAlias($banner['file'])), [
+            '{id}' => isset($banner['id']) ? $banner['id'] : uniqid(),
+            '{url}' => isset($banner['url']) ? $banner['url'] : '',
+        ])
+
+        ?>
     </div>
 
 <?php endfor; ?>
